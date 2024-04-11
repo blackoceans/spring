3d88d9c26651ad569964a2c37234812c5b940d67
blackoceans <blackoceans@naver.com>
2024-04-11 15:45:14 +0900

    0411

---
+++ BoardLab/WebContent/getBoardList.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>board list</h1>
+	<hr/>
+	<h3>${user.name }님 환영합니다. <a href="logout.do">logout</a></h3>
+	
+	<table border="1">
+		<tr>
+			<td>no</td><td>title</td><td>writer</td><td>date</td>
+		</tr>
+		<c:forEach var="board" items="${boardList }">
+		<tr>
+			<td>${board.seq }</td>
+			<td><a href="getBoard.do?seq=${board.seq }">${board.title }</a></td>
+			<td>${board.writer }</td>
+			<td>${board.regDate }</td>
+		</tr>
+		</c:forEach>
+	</table>
+	<br/>
+	<a href="insertBoard.html">add board</a>
+</body>
+</html>
--- BoardLab/src/biz/user/UserDAO.java
+++ BoardLab/src/biz/user/UserDAO.java
@@ -12,8 +12,11 @@ public class UserDAO {
 	private ResultSet rs;
 	
 	private static String USER_INSERT =
-			"insert into users (id, password, name, role) "+ //띄어쓰기 해야함 붙이면 오류 ) "
+			"insert into users (id, password, name, role) "+ //띄어쓰기 해야함 붙이면 오류 )여기"
 					"values (?,?,?,?)"; //뭐가 들어올지 몰라서 ?임 여기에 넣으면 각각에 들어감
+	private static String USER_GET =
+			"select * from users where id=? and password=?";
+					
 	
 	public void insertUser(UserVO vo) {
 		try {
@@ -29,5 +32,27 @@ public class UserDAO {
 		} finally {
 			JDBCUtil.close(stmt, conn);
 		}
+	}
+	public UserVO getUser(UserVO vo) {
+		UserVO user = null; //초기화
+		try {
+			conn = JDBCUtil.getConnetion();
+			stmt = conn.prepareStatement(USER_GET);
+			stmt.setNString(1, vo.getId());
+			stmt.setNString(2, vo.getPassword());
+			rs=stmt.executeQuery();
+			if(rs.next()) {
+				user = new UserVO();
+				user.setId(rs.getString("ID"));
+				user.setPassword(rs.getString("PASSWORD"));
+				user.setName(rs.getString("NAME"));
+				user.setRole(rs.getString("ROLE"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(rs, stmt, conn);
+		}
+		return user;
 	}
 }
